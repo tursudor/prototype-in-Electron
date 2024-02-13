2238dfa2645d14951f9cb5bbce9b5d84345063df
Tudor Domocos <tudor.domocos@yahoo.com>
2024-02-13 10:54:13 +0000

    linked project to github

--- forge.config.js
+++ forge.config.js
@@ -27,4 +27,17 @@ module.exports = {
       config: {},
     },
   ],
+  // publishers: [
+  //   {
+  //     name: '@electron-forge/publisher-github',
+  //     config: {
+  //       repository: {
+  //         owner: 'github-user-name',
+  //         name: 'github-repo-name'
+  //       },
+  //       prerelease: false,
+  //       draft: true
+  //     }
+  //   }
+  // ]
 };
